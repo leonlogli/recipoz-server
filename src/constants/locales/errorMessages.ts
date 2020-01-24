1b424ca1759276e811cb436f97844afec0b14fa5
Léon Logli <leonlogli@gmail.com>
2020-01-24 09:04:29 +0100

    Add utensil locales data

--- src/constants/locales/errorMessages.ts
+++ src/constants/locales/errorMessages.ts
@@ -1,18 +1,27 @@
 export default {
   category: {
     nameIsMandatory: 'categoryNameIsMandatory',
+    thumbnailIsMandatory: 'categoryThumbnailIsMandatory',
     notFound: 'categoryNotFound',
     updateNotFound: 'categoryToUpdateNotFound',
     deleteNotFound: 'categoryToDeleteNotFound'
   },
   ingredient: {
     nameIsMandatory: 'ingredientNameIsMandatory',
+    imageIsMandatory: 'ingredientImageIsMandatory',
     notFound: 'ingredientNotFound',
     updateNotFound: 'ingredientToUpdateNotFound',
     deleteNotFound: 'ingredientToDeleteNotFound'
   },
   measureUnit: {
     nameIsMandatory: 'measureUnitNameIsMandatory',
+    notFound: 'measureUnitNotFound',
+    updateNotFound: 'measureUnitToUpdateNotFound',
+    deleteNotFound: 'measureUnitToDeleteNotFound'
+  },
+  utensil: {
+    nameIsMandatory: 'measureUnitNameIsMandatory',
+    imageIsMandatory: 'utensilImageIsMandatory',
     notFound: 'measureUnitNotFound',
     updateNotFound: 'measureUnitToUpdateNotFound',
     deleteNotFound: 'measureUnitToDeleteNotFound'
