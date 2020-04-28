6e7c7208aa780bc3b9afdb12bf7c369cf7adda05
Léon Logli <leonlogli@gmail.com>
2020-04-28 06:13:54 +0000

    perf: improve error handling by remove unreachable errors

--- src/utils/errors/errorHelper.ts
+++ src/utils/errors/errorHelper.ts
@@ -3,16 +3,6 @@ import { GraphQLError } from 'graphql'
 import { ValidationError } from '@hapi/joi'
 import { UserInputError } from 'apollo-server-express'
 
-import {
-  errorMessages,
-  ID_TOKEN_EXPIRED,
-  ID_TOKEN_REVOKED,
-  ARGUMENT_ERROR,
-  INVALID_ID_TOKEN,
-  EMAIL_ALREADY_EXISTS,
-  PHONE_NUMBER_ALREADY_EXISTS,
-  USER_NOT_FOUND
-} from '../../constants'
 import { PROD_ENV, logger } from '../../config'
 import { ApiError } from './ApiError'
 import { AuthenticationError } from './AuthenticationError'
@@ -26,40 +16,6 @@ const statusCodeName = (code: StatusCode): string => {
   return (status as any)[_code]
 }
 
-const buildErrorDetails = (error: GraphQLError) => {
-  const { locations, path, extensions } = error
-  const { exception } = extensions || {}
-
-  return {
-    locations,
-    path,
-    exception
-  }
-}
-
-const formatFireBaseError = (error: GraphQLError) => {
-  const exception = error.extensions?.exception
-  const code = exception?.errorInfo.code
-  const { accessDenied, account } = errorMessages
-  const details = { ...buildErrorDetails(error) }
-
-  switch (code) {
-    case EMAIL_ALREADY_EXISTS:
-      return new ApiError(account.emailAlreadyExists, '409', details)
-    case PHONE_NUMBER_ALREADY_EXISTS:
-      return new ApiError(account.phoneNumberAlreadyExists, '409', details)
-    case USER_NOT_FOUND:
-      return new ApiError(account.userNotFound, '404', details)
-    case ID_TOKEN_EXPIRED:
-    case ID_TOKEN_REVOKED:
-    case INVALID_ID_TOKEN:
-    case ARGUMENT_ERROR:
-      return new ApiError(accessDenied, '401', details)
-    default:
-      return new ApiError(error.message, code, details)
-  }
-}
-
 const checkAndSendValidationErrors = (error?: ValidationError) => {
   if (error) {
     const validationErrors = error.details.map(detail => ({
@@ -68,35 +24,21 @@ const checkAndSendValidationErrors = (error?: ValidationError) => {
       type: detail.type
     }))
 
-    throw new UserInputError('Invalid input', {
-      validationErrors
-    })
+    throw new UserInputError('Invalid input', { validationErrors })
   }
 }
 
 /**
  * Runs on each error passed back to the client and format
- * some internal error message before sending them to the client
+ * internal errors message before sending them to the client
  * @param error graphql error
  */
 const formatError = (error: GraphQLError) => {
-  const { extensions } = error
-  const exception = extensions?.exception
   const code = error.extensions?.code
-  const { internalServerError } = errorMessages
+  const isInternalError = code === statusCodeName('500') || code === 500
 
-  if (exception?.codePrefix === 'auth') {
-    return formatFireBaseError(error)
-  }
-
-  if (code === statusCodeName('500')) {
-    if (exception?.name === 'CastError' && exception?.kind === 'ObjectId') {
-      return new UserInputError('Invalid id', buildErrorDetails(error))
-    }
-
-    if (PROD_ENV) {
-      return new ApiError(internalServerError, code, buildErrorDetails(error))
-    }
+  if (PROD_ENV && isInternalError) {
+    return new ApiError()
   }
 
   return error
@@ -114,17 +56,18 @@ const rewriteError = (error: GraphQLError) => {
     error instanceof ForbiddenError ||
     error instanceof UserInputError
   ) {
-    return null // do not report error
+    // do not report error
+    return null
   }
 
-  logger.error(error)
+  logger.error('', error)
 
-  return error // report the error.
+  // report the error.
+  return error
 }
 
 export {
   formatError,
-  buildErrorDetails,
   checkAndSendValidationErrors,
   rewriteError,
   statusCodeName
