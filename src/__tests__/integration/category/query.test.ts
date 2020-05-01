d8c9a673d81e07042c6b2783ea63d061fa8081bc
Léon Logli <leonlogli@gmail.com>
2020-05-01 01:33:47 +0000

    test(pagination): add bacward pagination integration tests

--- src/__tests__/integration/category/query.test.ts
+++ src/__tests__/integration/category/query.test.ts
@@ -24,54 +24,109 @@ describe('Category graph', () => {
     expect(res.data.node).to.equal(null)
   })
 
-  it('should fetch the first two categories', async () => {
-    const res = await client.useQuery(GET_CATEGORIES, { first: 2 })
+  describe('Forward pagination', () => {
+    it('should fetch the first two categories', async () => {
+      const res = await client.useQuery(GET_CATEGORIES, { first: 2 })
 
-    const { categories } = res.data
-    const expectPageInfo = { hasNextPage: true, hasPreviousPage: false }
+      const { categories } = res.data
+      const expectPageInfo = { hasNextPage: true, hasPreviousPage: false }
 
-    expect(categories.totalCount).to.equal(6)
-    expect(categories.pageInfo).to.include.keys('startCursor', 'endCursor')
-    expect(categories.edges).to.have.lengthOf(2)
-    expect(categories.edges[0]).to.have.keys('cursor', 'node')
-    expect(categories.edges[1]).to.have.keys('cursor', 'node')
-    expect(categories.nodes[0]).to.deep.include({ name: 'Vegetarian' })
-    expect(categories.nodes[1]).to.deep.include({ name: 'Togolese' })
-    expect(categories.pageInfo).to.include(expectPageInfo)
+      expect(categories.totalCount).to.equal(6)
+      expect(categories.pageInfo).to.include.keys('startCursor', 'endCursor')
+      expect(categories.edges).to.have.lengthOf(2)
+      expect(categories.edges[0]).to.have.keys('cursor', 'node')
+      expect(categories.edges[1]).to.have.keys('cursor', 'node')
+      expect(categories.nodes[0]).to.deep.include({ name: 'Vegetarian' })
+      expect(categories.nodes[1]).to.deep.include({ name: 'Togolese' })
+      expect(categories.pageInfo).to.include(expectPageInfo)
+    })
+
+    it('should fetch the next four categories with a cursor', async () => {
+      const { data } = await client.useQuery(GET_CATEGORIES, { first: 2 })
+      const after = data.categories.pageInfo.endCursor
+
+      const res = await client.useQuery(GET_CATEGORIES, { first: 4, after })
+
+      const { categories } = res.data
+      const expectPageInfo = { hasNextPage: false, hasPreviousPage: true }
+
+      expect(categories.totalCount).to.equal(6)
+      expect(categories.pageInfo).to.include.keys('startCursor', 'endCursor')
+      expect(categories.edges).to.have.lengthOf(4)
+      expect(categories.edges[0]).to.have.keys('cursor', 'node')
+      expect(categories.nodes[0]).to.deep.include({ name: 'Beninese' })
+      expect(categories.nodes[3]).to.deep.include({ name: 'Cuisine' })
+      expect(categories.pageInfo).to.include(expectPageInfo)
+    })
+
+    it('should fetch no categories at the end of connection', async () => {
+      const { data } = await client.useQuery(GET_CATEGORIES, { first: 20 })
+      const after = data.categories.pageInfo.endCursor
+
+      const res = await client.useQuery(GET_CATEGORIES, { first: 2, after })
+      const { categories } = res.data
+
+      const pageInfo = {
+        hasNextPage: false,
+        hasPreviousPage: true,
+        startCursor: null,
+        endCursor: null
+      }
+
+      expect(categories).to.deep.include({ nodes: [], edges: [], pageInfo })
+    })
   })
 
-  it('should fetch the next four categories with a cursor', async () => {
-    const { data } = await client.useQuery(GET_CATEGORIES, { first: 2 })
-    const after = data.categories.pageInfo.endCursor
+  describe('Backward pagination', () => {
+    it('should fetch the last two categories', async () => {
+      const res = await client.useQuery(GET_CATEGORIES, { last: 2 })
 
-    const res = await client.useQuery(GET_CATEGORIES, { first: 4, after })
+      const { categories } = res.data
+      const expectPageInfo = { hasNextPage: true, hasPreviousPage: false }
 
-    const { categories } = res.data
-    const expectPageInfo = { hasNextPage: false, hasPreviousPage: true }
+      expect(categories.totalCount).to.equal(6)
+      expect(categories.pageInfo).to.include.keys('startCursor', 'endCursor')
+      expect(categories.edges).to.have.lengthOf(2)
+      expect(categories.edges[0]).to.have.keys('cursor', 'node')
+      expect(categories.edges[1]).to.have.keys('cursor', 'node')
+      expect(categories.nodes[0]).to.deep.include({ name: 'Moroccan' })
+      expect(categories.nodes[1]).to.deep.include({ name: 'Cuisine' })
+      expect(categories.pageInfo).to.include(expectPageInfo)
+    })
 
-    expect(categories.totalCount).to.equal(6)
-    expect(categories.pageInfo).to.include.keys('startCursor', 'endCursor')
-    expect(categories.edges).to.have.lengthOf(4)
-    expect(categories.edges[0]).to.have.keys('cursor', 'node')
-    expect(categories.nodes[0]).to.deep.include({ name: 'Beninese' })
-    expect(categories.nodes[3]).to.deep.include({ name: 'Cuisine' })
-    expect(categories.pageInfo).to.include(expectPageInfo)
-  })
+    it('should fetch the last four categories with a cursor', async () => {
+      const { data } = await client.useQuery(GET_CATEGORIES, { last: 2 })
+      const before = data.categories.pageInfo.startCursor
 
-  it('should fetch no categories at the end of connection', async () => {
-    const { data } = await client.useQuery(GET_CATEGORIES, { first: 6 })
-    const after = data.categories.pageInfo.endCursor
+      const res = await client.useQuery(GET_CATEGORIES, { last: 4, before })
 
-    const res = await client.useQuery(GET_CATEGORIES, { first: 2, after })
-    const { categories } = res.data
+      const { categories } = res.data
+      const expectPageInfo = { hasNextPage: false, hasPreviousPage: true }
 
-    const pageInfo = {
-      hasNextPage: false,
-      hasPreviousPage: true,
-      startCursor: null,
-      endCursor: null
-    }
+      expect(categories.totalCount).to.equal(6)
+      expect(categories.pageInfo).to.include.keys('startCursor', 'endCursor')
+      expect(categories.edges).to.have.lengthOf(4)
+      expect(categories.edges[0]).to.have.keys('cursor', 'node')
+      expect(categories.nodes[0]).to.deep.include({ name: 'Vegetarian' })
+      expect(categories.nodes[3]).to.deep.include({ name: 'Breakfast' })
+      expect(categories.pageInfo).to.include(expectPageInfo)
+    })
 
-    expect(categories).to.deep.include({ nodes: [], edges: [], pageInfo })
+    it('should fetch no categories at the end of connection', async () => {
+      const { data } = await client.useQuery(GET_CATEGORIES, { last: 20 })
+      const before = data.categories.pageInfo.startCursor
+
+      const res = await client.useQuery(GET_CATEGORIES, { last: 2, before })
+      const { categories } = res.data
+
+      const pageInfo = {
+        hasNextPage: false,
+        hasPreviousPage: false,
+        startCursor: null,
+        endCursor: null
+      }
+
+      expect(categories).to.deep.include({ nodes: [], edges: [], pageInfo })
+    })
   })
 })
