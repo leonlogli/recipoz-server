fcecfccdcf2330d4a8b7b45d37ac8100dc89fcfd
Léon Logli <leonlogli@gmail.com>
2020-05-04 14:31:48 +0000

    test(integration): add account fetch test when the use is admin

--- src/__tests__/integration/account/query.test.ts
+++ src/__tests__/integration/account/query.test.ts
@@ -5,15 +5,15 @@ import { insertAccounts } from './util'
 import { GET_ACCOUNTS, ME } from './graph'
 
 describe('Account graph', () => {
-  let dbAccounts: any[] = []
+  let accounts: any[] = []
 
   beforeEach(async () => {
-    dbAccounts = await insertAccounts()
+    accounts = await insertAccounts()
   })
 
   describe('Account fecth', () => {
-    it('should throw authentication error if the current user is not admin', async () => {
-      const account = dbAccounts[0]
+    it('should throw access error if the current user is not admin', async () => {
+      const account = accounts[0]
       const ctx: any = { accountId: account._id, userRoles: ['USER'] }
 
       const res = await client.useQuery(GET_ACCOUNTS, undefined, ctx)
@@ -22,8 +22,20 @@ describe('Account graph', () => {
       expect(res.errors[0].extensions.code).to.deep.include('FORBIDDEN')
     })
 
+    it('should properly fetch accounts if the current user is admin', async () => {
+      const account = accounts[0]
+      const ctx: any = { accountId: account._id, userRoles: ['USER', 'ADMIN'] }
+
+      const res = await client.useQuery(GET_ACCOUNTS, undefined, ctx)
+      const { nodes, totalCount } = res.data.accounts
+
+      expect(totalCount).to.equal(4)
+      expect(nodes[0]).to.deep.include({ settings: { allergies: ['DAIRY'] } })
+      expect(nodes[3]).to.deep.include({ settings: { allergies: ['FISH'] } })
+    })
+
     it('should fetch the current user', async () => {
-      const account = dbAccounts[0]
+      const account = accounts[0]
       const ctx: any = { accountId: account._id, userRoles: ['USER'] }
 
       const res = await client.useQuery(ME, undefined, ctx)
--- src/__tests__/integration/account/util.ts
+++ src/__tests__/integration/account/util.ts
@@ -1,8 +1,21 @@
 import accounts from './accounts.json'
 import { Account } from '../../../models'
+import { client } from '../setup.test'
+import { toObjectId } from '../../../utils'
+import { GET_ACCOUNTS } from './graph'
 
 const insertAccounts = async () => {
   return Account.insertMany(accounts)
 }
 
-export { insertAccounts }
+const getDbAccounts = async () => {
+  const ctx: any = {
+    accountId: String(toObjectId(1)),
+    userRoles: ['ADMIN', 'USER']
+  }
+  const res = await client.useQuery(GET_ACCOUNTS, undefined, ctx)
+
+  return res.data.accounts.nodes
+}
+
+export { insertAccounts, getDbAccounts }
